45fefcfd324840cff0e210edb281ce19be454896
Hunter104 <000plagueinc@gmail.com>
2025-04-05 22:49:23 -0300

    Utiliza trigger para atualizar timestamp

--- monitor-db-physical.sql
+++ monitor-db-physical.sql
@@ -11,3 +11,10 @@ CREATE TABLE Website (
   last_status TEXT CHECK (last_status IN ('UP', 'DOWN', 'SLOW')),
   last_checked TEXT
 );
+
+CREATE TRIGGER Trg_lastChecked
+AFTER UPDATE ON Website
+FOR EACH ROW
+BEGIN 
+  UPDATE Website SET last_checked = CURRENT_TIMESTAMP WHERE id=OLD.id;
+END
